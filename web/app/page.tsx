076315d2ef2b061924ad8d503a451cba6585a0dd
hojin535 <123153695+hojin535@users.noreply.github.com>
2025-08-05 13:34:02 +0900

    Revert "feat:RN통신 테스트 코드"
    This reverts commit beaf049cdc4ed8946010efab5ccc8ece801b5406.

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,3 +1,4 @@
+
 import Link from 'next/link';
 
 export default function Home() {
@@ -5,9 +6,6 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen">
       <Link href="/login" className="text-blue-500 hover:underline">
         로그인 페이지로 이동
-      </Link>
-      <Link href="/test" className="text-blue-500 hover:underline">
-        RN통신 테스트 페이지
       </Link>
     </div>
   );
