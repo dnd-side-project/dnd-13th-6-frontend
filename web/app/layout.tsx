cdd509ea19355669817445587a51e71634dfaa46
hojin535 <123153695+hojin535@users.noreply.github.com>
2025-08-08 22:27:47 +0900

    feat:Lufga 폰트 추가

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -7,15 +7,113 @@ const pretendard = localFont({
   weight: '100 900',
   variable: '--font-pretendard',
 })
+
+const lufga = localFont({
+  src: [
+    {
+      path: '../fonts/Lufga/LufgaThin.ttf',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/Lufga/LufgaThinItalic.ttf',
+      weight: '100',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/Lufga/LufgaExtraLight.ttf',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/Lufga/LufgaExtraLightItalic.ttf',
+      weight: '200',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/Lufga/LufgaLight.ttf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/Lufga/LufgaLightItalic.ttf',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/Lufga/LufgaRegular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/Lufga/LufgaItalic.ttf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/Lufga/LufgaMedium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/Lufga/LufgaMediumItalic.ttf',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/Lufga/LufgaSemiBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/Lufga/LufgaSemiBoldItalic.ttf',
+      weight: '600',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/Lufga/LufgaBold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/Lufga/LufgaBoldItalic.ttf',
+      weight: '700',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/Lufga/LufgaExtraBold.ttf',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/Lufga/LufgaExtraBoldItalic.ttf',
+      weight: '800',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/Lufga/LufgaBlack.ttf',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/Lufga/LufgaBlackItalic.ttf',
+      weight: '900',
+      style: 'italic',
+    },
+  ],
+  display: 'swap',
+  variable: '--font-lufga',
+})
+
 export default function RootLayout({
                                      children,
                                    }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${lufga.variable} ${pretendard.variable}`}>
     <body
-      className={`${pretendard.variable} antialiased`}
+      className={`${lufga.className} antialiased`}
     >
     {children}
     </body>
