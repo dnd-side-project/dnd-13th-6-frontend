139c16c17cec448474b8fbadf5c688fb09654ed6
hojin535 <123153695+hojin535@users.noreply.github.com>
2025-08-09 16:49:25 +0900

    feat:tailwind 설정

---
+++ web/components/personal-map/personalMapPage.tsx
@@ -0,0 +1,13 @@
+'use client';
+import React from 'react';
+import KakaoMap from '@/components/KakaoMap/KakaoMap';
+
+function PersonalMapPage() {
+  return (
+    <>
+      <KakaoMap height="50vh" />
+    </>
+  );
+}
+
+export default PersonalMapPage;
