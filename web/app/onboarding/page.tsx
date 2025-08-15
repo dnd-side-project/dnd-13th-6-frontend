1506df303e255a322f9674ea73437995641fbf36
hojin535 <123153695+hojin535@users.noreply.github.com>
2025-08-15 14:05:26 +0900

    feat:온보딩 구현

---
+++ web/app/onboarding/page.tsx
@@ -0,0 +1,75 @@
+'use client';
+import React, { useState } from 'react';
+import Button from '@/components/common/Button';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+function Page() {
+  const title = [
+    '지금 친구가 \n어디쯤 달리고 있을까?',
+    '우리팀 목표, \n세계의 행운이 함께 응원해요!',
+    '같이 달리는 기쁨,\n지금 여기서 시작해요!'
+  ];
+  const text = [
+    '실시간으로 함께 느끼는 러닝, 이제 당신 차례에요',
+    '각 나라의 행운 상징에서 탄생한 캐릭터들과 \n목표를 달성하고, 클로버 보상까지 받아보세요!'
+  ];
+  const image = ['img1', 'img2', 'img3'];
+  const [index, setIndex] = useState(0);
+  const router = useRouter();
+  return (
+    <>
+      {/* 글 영역 - 위쪽 */}
+      <div className="ml-3 mt-10">
+        <p className="text-gray-20 text-2xl font-bold whitespace-pre-line">
+          {title[index]}
+        </p>
+        <p className="whitespace-pre-line text-base text-gray-60 mt-1">
+          {text[index]}
+        </p>
+      </div>
+
+      {/* 이미지 + 점 - 화면 정중앙 */}
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center gap-4">
+        <Image
+          src={`/assets/onboarding/${image[index]}.png`}
+          alt={image[index]}
+          width={243}
+          height={243}
+          priority
+          className="object-contain"
+        />
+        <div className="flex gap-2">
+          <div
+            className={`h-3 rounded-full  ${index === 0 ? 'bg-primary w-8' : 'bg-gray-60 w-3 '}`}
+          />
+          <div
+            className={`w-3 h-3 rounded-full  ${index === 1 ? 'bg-primary w-8' : 'bg-gray-60 w-3 '}`}
+          />
+          <div
+            className={`w-3 h-3 rounded-full  ${index === 2 ? 'bg-primary w-8' : 'bg-gray-60 w-3 '}`}
+          />
+        </div>
+      </div>
+
+      {/* 버튼 */}
+      {index !== 2 ? (
+        <button
+          className="absolute bottom-15 right-5 text-gray-60"
+          onClick={() => setIndex(index + 1)}
+        >
+          건너뛰기
+        </button>
+      ) : (
+        <Button
+          className="absolute bottom-15 h-15 left-5 right-5"
+          onClick={() => router.replace('/login')}
+        >
+          시작하기
+        </Button>
+      )}
+    </>
+  );
+}
+
+export default Page;
