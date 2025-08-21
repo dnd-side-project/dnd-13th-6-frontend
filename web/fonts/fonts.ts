18a28b2780398cf863da841d697d2c4a62b514f2
hojin535 <123153695+hojin535@users.noreply.github.com>
2025-08-21 18:02:49 +0900

    chore: 폰트 상대 경로 수정

--- web/fonts/fonts.ts
+++ web/fonts/fonts.ts
@@ -1,7 +1,7 @@
 import localFont from 'next/font/local';
 
 export const pretendard = localFont({
-  src: '../fonts/pretendard/PretendardVariable.woff2',
+  src: '/pretendard/PretendardVariable.woff2',
   display: 'swap',
   weight: '100 900',
   variable: '--font-pretendard'
@@ -10,92 +10,92 @@ export const pretendard = localFont({
 export const lufga = localFont({
   src: [
     {
-      path: '../fonts/Lufga/LufgaThin.ttf',
+      path: '/Lufga/LufgaThin.ttf',
       weight: '100',
       style: 'normal'
     },
     {
-      path: '../fonts/Lufga/LufgaThinItalic.ttf',
+      path: '/Lufga/LufgaThinItalic.ttf',
       weight: '100',
       style: 'italic'
     },
     {
-      path: '../fonts/Lufga/LufgaExtraLight.ttf',
+      path: '/Lufga/LufgaExtraLight.ttf',
       weight: '200',
       style: 'normal'
     },
     {
-      path: '../fonts/Lufga/LufgaExtraLightItalic.ttf',
+      path: '/Lufga/LufgaExtraLightItalic.ttf',
       weight: '200',
       style: 'italic'
     },
     {
-      path: '../fonts/Lufga/LufgaLight.ttf',
+      path: '/Lufga/LufgaLight.ttf',
       weight: '300',
       style: 'normal'
     },
     {
-      path: '../fonts/Lufga/LufgaLightItalic.ttf',
+      path: '/Lufga/LufgaLightItalic.ttf',
       weight: '300',
       style: 'italic'
     },
     {
-      path: '../fonts/Lufga/LufgaRegular.ttf',
+      path: '/Lufga/LufgaRegular.ttf',
       weight: '400',
       style: 'normal'
     },
     {
-      path: '../fonts/Lufga/LufgaItalic.ttf',
+      path: '/Lufga/LufgaItalic.ttf',
       weight: '400',
       style: 'italic'
     },
     {
-      path: '../fonts/Lufga/LufgaMedium.ttf',
+      path: '/Lufga/LufgaMedium.ttf',
       weight: '500',
       style: 'normal'
     },
     {
-      path: '../fonts/Lufga/LufgaMediumItalic.ttf',
+      path: '/Lufga/LufgaMediumItalic.ttf',
       weight: '500',
       style: 'italic'
     },
     {
-      path: '../fonts/Lufga/LufgaSemiBold.ttf',
+      path: '/Lufga/LufgaSemiBold.ttf',
       weight: '600',
       style: 'normal'
     },
     {
-      path: '../fonts/Lufga/LufgaSemiBoldItalic.ttf',
+      path: '/Lufga/LufgaSemiBoldItalic.ttf',
       weight: '600',
       style: 'italic'
     },
     {
-      path: '../fonts/Lufga/LufgaBold.ttf',
+      path: '/Lufga/LufgaBold.ttf',
       weight: '700',
       style: 'normal'
     },
     {
-      path: '../fonts/Lufga/LufgaBoldItalic.ttf',
+      path: '/Lufga/LufgaBoldItalic.ttf',
       weight: '700',
       style: 'italic'
     },
     {
-      path: '../fonts/Lufga/LufgaExtraBold.ttf',
+      path: '/Lufga/LufgaExtraBold.ttf',
       weight: '800',
       style: 'normal'
     },
     {
-      path: '../fonts/Lufga/LufgaExtraBoldItalic.ttf',
+      path: '/Lufga/LufgaExtraBoldItalic.ttf',
       weight: '800',
       style: 'italic'
     },
     {
-      path: '../fonts/Lufga/LufgaBlack.ttf',
+      path: '/Lufga/LufgaBlack.ttf',
       weight: '900',
       style: 'normal'
     },
     {
-      path: '../fonts/Lufga/LufgaBlackItalic.ttf',
+      path: '/Lufga/LufgaBlackItalic.ttf',
       weight: '900',
       style: 'italic'
     }
