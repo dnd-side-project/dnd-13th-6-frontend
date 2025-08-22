666f050f6d3f4fdb8a113983c026c4fd4f9ec17a
yjh1008 <exesea12@gmail.com>
2025-08-23 02:32:19 +0900

    feat: 크루 조회 페이지 UI 구현

---
+++ web/app/group-code/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Page() {
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <div className="text-white font-bold text-lg">초대 코드를 입력해주세요.</div>
+    </div>
+  );
+}
--- web/app/group/page.tsx
+++ web/app/group/page.tsx
@@ -1,9 +1,10 @@
 'use client'
 import Image from 'next/image'
 import ProgressBar from "@/components/common/ProgressBar"
+import { useRouter } from 'next/navigation'
 function CrewCard() {
   return (
-    <div className="bg-[#363538] px-4 py-[22px] rounded-2xl flex flex-col gap-4">
+    <div className="bg-[#363538] px-4 py-[22px] rounded-2xl flex flex-col gap-4 flex-grow">
       <div className="flex w-full justify-between items-center">
         <div>
           <div>블랙핑크-뛰어<Image className='inline' src="/assets/CaretRight.svg" alt="crew" width={20} height={20} /> </div>
@@ -18,16 +19,31 @@ function CrewCard() {
       </div>
       <ProgressBar progress={50} className="w-full" />
       <button className="w-full bg-[#48484A] rounded-2xl" onClick={() => alert('결과보기')}>
-        <div className="text-[17px] py-3 text-bold">이번 주 결과보기</div>
+        <div className="text-[17px] py-3 font-bold">이번 주 결과보기</div>
       </button>
     </div>
   )
 }
 
 export default function Page() {
+  const router = useRouter();
   return (
     <div className="w-full">
-      <CrewCard />
+      <div className="flex flex-col gap-4 h-[calc(100vh-90px)] overflow-y-auto">
+        <CrewCard />
+        <CrewCard />
+        <CrewCard />
+        <CrewCard />
+        <CrewCard />
+      </div>
+      <div className="w-full flex gap-3 fixed bottom-4">
+        <button className="w-full bg-[#48484A] rounded-2xl" onClick={() => alert('결과보기')}>
+          <div className="text-[17px] py-3 font-bold">크루 만들기</div>
+        </button>
+        <button className="w-full bg-[#32FF76] rounded-2xl" onClick={() => router.push('/group-code')}>
+          <div className="text-[18px] py-3 text-black font-bold">초대 코드 입력</div>
+        </button>
+      </div>
     </div>
   )
 }
