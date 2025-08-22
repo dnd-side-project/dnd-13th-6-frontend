d4b8de0dcd02b26841f1b7dc7e1046678d9e084e
hojin535 <123153695+hojin535@users.noreply.github.com>
2025-08-22 19:48:02 +0900

    feat: 개인 러닝 성공 실패화면 구현

--- web/app/(personal-run)/lucky-stamp/page.tsx
+++ web/app/(personal-run)/lucky-stamp/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '@/components/common/Button';
 import { useRouter } from 'next/navigation';
 
 export default function LuckyStampPage() {
+  const [isSucess, setIsSucess] = useState(false);
   const [visible, setVisible] = useState(false);
   const router = useRouter();
   useEffect(() => {
@@ -14,33 +15,59 @@ export default function LuckyStampPage() {
   }, []);
 
   return (
-    <div className="text-white min-h-screen flex flex-col justify-between p-4 ">
-      {/* Spacer for top area */}
-      <h1 className="text-2xl font-semibold leading-relaxed mb-8 whitespace-pre-line">
-        {'오늘도 수고 많으셨어요!\n오늘의 행운이 도착했어요!'}
-      </h1>
-      <div className="flex-grow flex flex-col items-center justify-center text-center">
-        <Image
-          src="/assets/lucky-stamp/four-leaf-clover.png"
-          alt="Four Leaf Clover"
-          width={225}
-          height={224}
-          priority
-        />
-        <div
-          className={`inline-flex items-center justify-center px-6 py-4
-      bg-[rgba(255,255,255,0.13)] rounded-[20px] mt-10 transition-opacity duration-1000 ${
-        visible ? 'opacity-100' : 'opacity-0'
-      }`}
-        >
-          <p className="text-xl font-bold text-gray-20 ">클로버 +1개</p>
-        </div>
-      </div>
+    <div className="relative flex h-full flex-col items-center bg-[#201F22] text-center text-white">
+      <div className="flex-grow pt-10">
+        <h1 className="onboarding whitespace-pre-line">
+          {isSucess
+            ? '오늘도 수고 많으셨어요!\n오늘의 행운이 도착했어요!'
+            : '클로버는 놓쳤지만\n꾸준함이 곧 행운이에요!'}
+        </h1>
 
-      {/* Bottom Button */}
-      <div className="w-full">
-        <Button onClick={() => router.push('/')} className="w-full h-15 mb-5">
-          홈화면으로 돌아가기
+        <div className="relative">
+          <Image
+            src="/assets/common/backgroundLight.svg"
+            alt="배경"
+            width={325}
+            height={325}
+            className={`${!isSucess && 'invisible'} object-contain`}
+          />
+          {isSucess ? (
+            <Image
+              src="/assets/lucky-stamp/four-leaf-clover.svg"
+              alt="Four Leaf Clover"
+              width={225}
+              height={224}
+              priority
+              className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+            />
+          ) : (
+            <Image
+              src="/assets/lucky-stamp/disabled-four-leaf-clover.svg"
+              alt="disabledFour Leaf Clover"
+              width={225}
+              height={224}
+              priority
+              className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+            />
+          )}
+        </div>
+        {isSucess && (
+          <div
+            className={`relative mx-auto inline-flex items-center justify-center rounded-[20px] bg-white/13 px-5 py-1 transition-opacity duration-1000 ${visible ? 'opacity-100' : 'opacity-0'}`}
+          >
+            <p className="pretendard-headline text-gray-20">
+              야호! 야생의 클로버를 +1개 얻었다!
+            </p>
+          </div>
+        )}
+      </div>
+      <div className="w-full pb-9">
+        <p className="text-gray-60 font-regular mb-5 text-sm text-[0.9375rem] tracking-[-0.014em]">
+          현재 <span className="text-golden">7/10</span>개!{' '}
+          <span className="text-golden">3</span>개만 더 모으면 가챠 도전 가능!
+        </p>
+        <Button onClick={() => router.push('/main')} className="h-15 w-full">
+          홈 화면으로 돌아가기
         </Button>
       </div>
     </div>
