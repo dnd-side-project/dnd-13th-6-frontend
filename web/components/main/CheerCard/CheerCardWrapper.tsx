6a40d6d453b6af924e2e665f73124ebc63347503
김호진 <hojin535@naver.com>
2025-08-23 16:37:12 +0900

    refactor: &&대신 switch로 변경

--- web/components/main/CheerCard/CheerCardWrapper.tsx
+++ web/components/main/CheerCard/CheerCardWrapper.tsx
@@ -7,18 +7,28 @@ import NoActiveRunner from '@/components/main/CheerCard/NoActiveRunner';
 import TwoActiveRunner from '@/components/main/CheerCard/TwoActiveRunner';
 import ManyActiveRunner from '@/components/main/CheerCard/ManyActiveRunner';
 
-const CheerCard = () => {
-  const [count] = useState(3);
+const CheerCardWrapper = () => {
+  const [count] = useState(0);
+  const renderRunnerComponent = () => {
+    switch (count) {
+      case 0:
+        return <NoActiveRunner />;
+      case 1:
+        return <OneActiveRunner />;
+      case 2:
+        return <TwoActiveRunner />;
+      default:
+        return count >= 3 ? <ManyActiveRunner /> : null;
+    }
+  };
+
   return (
     <Card className="relative overflow-hidden">
       <div className="absolute inset-0 z-10 bg-[linear-gradient(180deg,rgba(32,31,34,0.82)_0%,rgba(32,31,34,0.6)_100%)]" />
       <Image src="/assets/main/map.png" alt="map" fill className="z-0" />
-      {count === 0 && <NoActiveRunner />}
-      {count === 1 && <OneActiveRunner />}
-      {count === 2 && <TwoActiveRunner />}
-      {count >= 3 && <ManyActiveRunner />}
+      {renderRunnerComponent()}
     </Card>
   );
 };
 
-export default CheerCard;
+export default CheerCardWrapper;
