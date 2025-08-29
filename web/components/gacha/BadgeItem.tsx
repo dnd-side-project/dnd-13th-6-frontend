709aa75d14c058aedaafdb9b93ff530c7691179d
hojin535 <123153695+hojin535@users.noreply.github.com>
2025-08-30 03:31:00 +0900

    style:선택된 뱃지 border

--- web/components/gacha/BadgeItem.tsx
+++ web/components/gacha/BadgeItem.tsx
@@ -2,9 +2,16 @@ import React from 'react';
 import Card from '@/components/main/Card';
 import Image from 'next/image';
 
-function BadgeItem({ badge }: { badge: string }) {
+function BadgeItem({ badge, badgeUrl }: { badge: string; badgeUrl: string }) {
   return (
-    <Card className="flex h-[114px] w-[114px] items-center justify-center p-[7px]">
+    <Card
+      className={`\`relative flex h-[114px] w-[114px] items-center justify-center p-[7px] ${
+        badgeUrl === badge
+          ? 'shadow-[0_0_0_1px_theme(colors.primary)] rounded-3xl'
+          : ''
+      }\``}
+    >
+      className={}
       <Image
         src={badge}
         alt="캐릭터"
