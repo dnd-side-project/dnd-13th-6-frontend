808b8cfbef4db88b0841d5f04440e4abf3ce8320
yjh1008 <exesea12@gmail.com>
2025-09-08 00:27:12 +0900

    design: 디자인 시스템 코드 등록

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -9,12 +9,25 @@ module.exports = {
         black: '#1C1C1E',
         white: '#fff',
         gray: '#313131',
+        gray10: '#F2F2F7',
         gray20: '#E5E5EA',
         gray40: '#C7C7CC',
+        gray50: '#AEAEB2',
         gray60: '#8E8E93',
+        gray80: '#48484A',
         gray70: '#636366',
         gray90: '#3A3A3C',
-        red:'#ff0000'
+        red:'#ff0000',
+        orange: '#FF9500',
+        yellow: '#FFCC00',
+        green: '#34C759',
+        mint: '#00C7BE',
+        teal: '#5AC8FA',
+        cyan: '#5AC8FA',
+        blue: '#007AFF',
+        purple: '#AF52DE',
+        pink: '#FF2D92',
+        brown: '#A2845E'
       },
       fontFamily: {
         pretendard: ['Pretendard-Regular', 'sans-serif'],
@@ -24,9 +37,11 @@ module.exports = {
       },
       fontSize: {
         title3: ['22px', {fontWeight:'bold'}],
-        headline1: ['17px', { lineHeight: '24px', fontWeight: 'bold' }],
-        body1: ['17px', { lineHeight: '24px', fontWeight: '500' }],
-        body2: ['15px', { lineHeight: '22px', fontWeight: '500' }],
+        headline: ['19px', { lineHeight: '150%', fontWeight: 'medium', letterSpacing: '-0.014em' }],
+        headline1: ['17px', { lineHeight: '150%', fontWeight: 'bold', letterSpacing: '-0.014em' }],
+        headline2: ['15px', { lineHeight: '150%', fontWeight: 'semibold', letterSpacing: '-0.014em' }],
+        body1: ['17px', { lineHeight: '150%', fontWeight: 'regular', letterSpacing: '-0.025em' }],
+        body2: ['15px', { lineHeight: '150%', fontWeight: 'regular', letterSpacing: '-0.025em' }],
         body3: ['13px', { lineHeight: '18px', fontWeight: '500' }],
         body4: ['11px', { lineHeight: '16px', fontWeight: '500' }]
       }
