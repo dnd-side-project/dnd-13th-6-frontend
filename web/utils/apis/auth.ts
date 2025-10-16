e87079f00cc60032ebe2fc9d1201a80d6be21631
hojin535 <hojin535@naver.com>
2025-10-16 10:30:18 +0900

    fix: 헤더,토큰으로 분리해놨던 로직 헤더 전달로 통일

--- web/utils/apis/auth.ts
+++ web/utils/apis/auth.ts
@@ -17,19 +17,15 @@ export const tokenRefresh = async () => {
 };
 
 export const logout = async () => {
-  if (isDev) {
-    await api.post(
-      `${AUTH_API.LOG_OUT()}`,
-      {},
-      {
-        headers: {
-          'X-Refresh-Token': localStorage.getItem('refreshToken') || ''
-        }
+  await api.post(
+    `${AUTH_API.LOG_OUT()}`,
+    {},
+    {
+      headers: {
+        'X-Refresh-Token': localStorage.getItem('refreshToken') || ''
       }
-    );
-  } else {
-    await api.post(AUTH_API.LOG_OUT());
-  }
+    }
+  );
 };
 export const withdraw = async () => {
   await api.delete(MEMBER_API.WITHDRAW());
