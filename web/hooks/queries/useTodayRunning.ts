3e965ae725f22238027b42e661a820a2af74fdc6
hojin535 <hojin535@naver.com>
2025-10-16 19:47:14 +0900

    fix: accessToken 유무에 따라 쿼리 활성화 및 코드 정리

--- web/hooks/queries/useTodayRunning.ts
+++ web/hooks/queries/useTodayRunning.ts
@@ -3,9 +3,12 @@ import { queryKeys } from '@/utils/queries/queryKeys';
 import { getTodayRunning } from '@/utils/apis/running';
 
 export const useTodayRunning = () => {
+  const hasToken = typeof window !== 'undefined' && !!localStorage.getItem('accessToken');
+
   return useQuery({
     queryKey: queryKeys.running.today(),
     queryFn: getTodayRunning,
+    enabled: hasToken,
     gcTime: 10 * 60 * 1000,
     staleTime: 5 * 60 * 1000
   });
--- web/hooks/queries/useUserInfo.ts
+++ web/hooks/queries/useUserInfo.ts
@@ -4,9 +4,12 @@ import { getUserInfo } from '@/utils/apis/member';
 import { UserInfo } from '@/types/userInfo';
 
 export const useUserInfo = () => {
+  const hasToken = typeof window !== 'undefined' && !!localStorage.getItem('accessToken');
+
   return useQuery<UserInfo>({
     queryKey: queryKeys.member.info(),
     queryFn: getUserInfo,
+    enabled: hasToken,
     gcTime: 10 * 60 * 1000,
     staleTime: 5 * 60 * 1000
   });
