f2ab72c15b2360cba20cdf3dfa8727b7f109acf8
yjh1008 <exesea12@gmail.com>
2025-11-21 01:41:26 +0900

    fix: login url 수정

--- web/app/login/page.tsx
+++ web/app/login/page.tsx
@@ -15,7 +15,7 @@ export default function Login() {
       <Link
         href={
           process.env.NEXT_PUBLIC_LOGIN_URL ||
-          'https://kauth.kakao.com/oauth/authorize?client_id=3255efd2af839833b26a422ca203c180&redirect_uri=https://api.runky.store/api/auth/dev/login/oauth2/code/kakao&response_type=code'
+          `https://kauth.kakao.com/oauth/authorize?client_id=3255efd2af839833b26a422ca203c180&redirect_uri=https://api.runky.store/api/auth/login/oauth2/code/kakao&response_type=code`
         }
         className="absolute top-1/2 left-1/2 h-[13vw] max-h-[100px] w-[calc(100vw-5vw)] max-w-[400px] -translate-x-1/2 -translate-y-1/2"
       >
@@ -30,7 +30,7 @@ export default function Login() {
       <Link
         href={
           process.env.NEXT_PUBLIC_LOGIN_URL ||
-          'https://appleid.apple.com/auth/authorize?client_id=api.runky.store&redirect_uri=https://api.runky.store/api/auth/dev/login/oauth2/code/apple&response_type=code&response_mode=form_post&scope=name%20email'
+          `https://appleid.apple.com/auth/authorize?client_id=api.runky.store&redirect_uri=https://api.runky.store/api/auth/login/oauth2/code/apple&response_type=code&response_mode=form_post&scope=name%20email`
         }
         className="absolute top-[60%] px-2 flex items-center rounded-lg left-1/2 h-[13vw] max-h-[100px] w-[calc(100vw-12vw)] max-w-[400px] -translate-x-1/2 -translate-y-1/2 bg-white text-black"
       >
