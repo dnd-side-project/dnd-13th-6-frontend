5da14c11526edf829891d96cffeb864e3700190c
yjh1008 <exesea12@gmail.com>
2025-11-21 21:47:13 +0900

    fix: 카카오 로그인 ui 수정

--- web/app/login/page.tsx
+++ web/app/login/page.tsx
@@ -12,27 +12,28 @@ export default function Login() {
       </div>
 
       {/* 로그인 버튼 - 화면 정중앙 */}
+      <div className="flex flex-col items-center justify-between  absolute top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2 gap-10">
       <Link
         href={
           process.env.NEXT_PUBLIC_LOGIN_URL ||
           `https://kauth.kakao.com/oauth/authorize?client_id=3255efd2af839833b26a422ca203c180&redirect_uri=https://api.runky.store/api/auth/login/oauth2/code/kakao&response_type=code`
         }
-        className="absolute top-1/2 left-1/2 h-[13vw] max-h-[100px] w-[calc(100vw-5vw)] max-w-[400px] -translate-x-1/2 -translate-y-1/2"
+        className="flex items-center  h-[13vw] max-h-[60px] rounded-lg px-3 w-[calc(100vw-5vw)] max-w-[400px]  !bg-[#FEE500] text-black font-semibold text-lg"
       >
         <Image
-          src="/assets/kakao_login_large_wide.png"
+          src="/assets/kakao-icon.svg"
           alt="카카오 로그인"
-          fill
-          priority
-          className="object-contain"
+          width={24}
+          height={24}
         />
+         <div className='text-center flex-grow mr-5'> 카카오 로그인</div>
       </Link>
       <Link
         href={
           process.env.NEXT_PUBLIC_LOGIN_URL ||
           `https://appleid.apple.com/auth/authorize?client_id=api.runky.store&redirect_uri=https://api.runky.store/api/auth/login/oauth2/code/apple&response_type=code&response_mode=form_post&scope=name%20email`
         }
-        className="absolute top-[60%] flex items-center left-1/2 h-[13vw] max-h-[60px] rounded-lg px-2 w-[calc(100vw-5vw)] max-w-[400px] -translate-x-1/2 -translate-y-1/2 bg-white text-black font-semibold text-lg"
+        className="flex items-center  h-[13vw] max-h-[60px] rounded-lg px-2 w-[calc(100vw-5vw)] max-w-[400px]  bg-white text-black font-semibold text-lg"
       >
         
         <Image
@@ -44,6 +45,8 @@ export default function Login() {
         />
         <div className='text-center flex-grow mr-5'> 애플 로그인</div>
       </Link>
+
+      </div>
     </div>
   );
 }
